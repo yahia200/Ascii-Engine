ec0761ec2eedeffed4da42901e9d365a01432a13
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-06-06 04:42:48 +0300

    proj mat

--- engine.c
+++ engine.c
@@ -1,23 +1,39 @@
+#include <math.h>
 #include <ncurses.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define LIGHT 0
-#define MEDIUM 1
+#define MEDIUM 2
 #define DARK 2
 #define MAX_MESH_SIZE 20
-#define MAX_WIDTH 237
-#define MAX_HIEGHT 237
+#define MAX_WIDTH 50
+#define MAX_HIEGHT 20
 char brightness[] = ".*@";
 
-struct Point {
-  int x, y;
+struct mat {
+  float m[4][4];
 };
 
+struct mat matProj, matRotZ, matRotX;
+
+struct Point {
+  float x, y, z;
+};
+
+void MultiplyMatrixVector(struct Point i, struct Point o, struct mat m) {
+  o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+  o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+  o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+  float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+  if (w != 0.0f) {
+    o.x /= w;
+    o.y /= w;
+    o.z /= w;
+  }
+}
+
 struct Triangle {
-  struct Point p1;
-  struct Point p2;
-  struct Point p3;
+  struct Point p[3];
 };
 
 struct Mesh {
@@ -26,16 +42,15 @@ struct Mesh {
 };
 
 void drawPoint(struct Point point, int level) {
-  if (point.x >= 0 && point.x < MAX_WIDTH && point.y >= 0 && point.y < MAX_HIEGHT) {
-    move(point.y, point.x);
-    addch('*');
-    //mvwaddch(stdscr, point.y, point.x, brightness[level]);
+  if (point.x >= 0 && point.x < MAX_WIDTH && point.y >= 0 &&
+      point.y < MAX_HIEGHT) {
+    mvwaddch(stdscr, point.y, point.x, brightness[level]);
   }
 }
 
 void drawLine(struct Point p1, struct Point p2) {
-  int dx = abs(p2.x - p1.x), sx = p1.x < p2.x ? 1 : -1;
-  int dy = -abs(p2.y - p1.y), sy = p1.y < p2.y ? 1 : -1;
+  int dx = fabsf(p2.x - p1.x), sx = p1.x < p2.x ? 1 : -1;
+  int dy = -fabsf(p2.y - p1.y), sy = p1.y < p2.y ? 1 : -1;
   int err = dx + dy, e; /* error value e_xy */
 
   for (;;) {
@@ -55,9 +70,59 @@ void drawLine(struct Point p1, struct Point p2) {
 }
 
 void drawTriangle(struct Triangle triangle) {
-  drawLine(triangle.p1, triangle.p2);
-  drawLine(triangle.p2, triangle.p3);
-  drawLine(triangle.p3, triangle.p1);
+  struct Triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+  move(0, 0);
+  printw("%f    %f |%f    %f|%f    %f", triangle.p[0].x, triangle.p[0].y,
+         triangle.p[1].x, triangle.p[1].y, triangle.p[2].x, triangle.p[2].y);
+  MultiplyMatrixVector(triangle.p[0], triRotatedZ.p[0], matRotZ);
+  MultiplyMatrixVector(triangle.p[1], triRotatedZ.p[1], matRotZ);
+  MultiplyMatrixVector(triangle.p[2], triRotatedZ.p[2], matRotZ);
+  move(1, 0);
+  printw("%f    %f |%f    %f|%f    %f", triRotatedZ.p[0].x, triRotatedZ.p[0].y,
+         triRotatedZ.p[1].x, triRotatedZ.p[1].y, triRotatedZ.p[2].x,
+         triRotatedZ.p[2].y);
+  MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+  MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+  MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+  move(2, 0);
+  printw("%f    %f |%f    %f|%f    %f", triRotatedZX.p[0].x,
+         triRotatedZX.p[0].y, triRotatedZX.p[1].x, triRotatedZX.p[1].y,
+         triRotatedZX.p[2].x, triRotatedZX.p[2].y);
+  triTranslated = triRotatedZX;
+  triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+  triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+  triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+  move(3, 0);
+  printw("%f    %f |%f    %f|%f    %f", triTranslated.p[0].x,
+         triTranslated.p[0].y, triTranslated.p[1].x, triTranslated.p[1].y,
+         triTranslated.p[2].x, triTranslated.p[2].y);
+  MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+  MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+  MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+  move(4, 0);
+  printw("%f    %f |%f    %f|%f    %f", triProjected.p[0].x,
+         triProjected.p[0].y, triProjected.p[1].x, triProjected.p[1].y,
+         triProjected.p[2].x, triProjected.p[2].y);
+  triProjected.p[0].x += 1.0f;
+  triProjected.p[0].y += 1.0f;
+  triProjected.p[1].x += 1.0f;
+  triProjected.p[1].y += 1.0f;
+  triProjected.p[2].x += 1.0f;
+  triProjected.p[2].y += 1.0f;
+  triProjected.p[0].x *= 0.5f * (float)MAX_WIDTH;
+  triProjected.p[0].y *= 0.5f * (float)MAX_HIEGHT;
+  triProjected.p[1].x *= 0.5f * (float)MAX_WIDTH;
+  triProjected.p[1].y *= 0.5f * (float)MAX_HIEGHT;
+  triProjected.p[2].x *= 0.5f * (float)MAX_WIDTH;
+  triProjected.p[2].y *= 0.5f * (float)MAX_HIEGHT;
+  move(5, 0);
+  printw("%f    %f |%f    %f|%f    %f", triProjected.p[0].x,
+         triProjected.p[0].y, triProjected.p[1].x, triProjected.p[1].y,
+         triProjected.p[2].x, triProjected.p[2].y);
+  getch();
+  drawLine(triProjected.p[0], triProjected.p[1]);
+  drawLine(triProjected.p[1], triProjected.p[2]);
+  drawLine(triProjected.p[2], triProjected.p[0]);
 }
 
 void drawMesh(struct Mesh mesh) {
@@ -71,9 +136,56 @@ void clearScreen() {
   clear();
 }
 
-void initScreen(){
+void initScreen() {
   initscr();
   noecho();
   curs_set(0);
+}
 
+void fillMat(struct mat m) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      m.m[i][j] = 0;
+    }
+  }
+}
+
+void initMat() {
+  float fNear = 0.1f;
+  float fFar = 1000.0f;
+  float fFov = 90.0f;
+  float fAspectRatio = (float)MAX_HIEGHT / (float)MAX_WIDTH;
+  float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+  float fTheta = 0.0f;
+  fillMat(matProj);
+  matProj.m[0][0] = fAspectRatio * fFovRad;
+  matProj.m[1][1] = fFovRad;
+  matProj.m[2][2] = fFar / (fFar - fNear);
+  matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+  matProj.m[2][3] = 1.0f;
+  matProj.m[3][3] = 0.0f;
+  fillMat(matRotZ);
+  matRotZ.m[0][0] = cosf(fTheta);
+  matRotZ.m[0][1] = sinf(fTheta);
+  matRotZ.m[1][0] = -sinf(fTheta);
+  matRotZ.m[1][1] = cosf(fTheta);
+  matRotZ.m[2][2] = 1;
+  matRotZ.m[3][3] = 1;
+  fillMat(matRotX);
+  matRotX.m[0][0] = 1;
+  matRotX.m[1][1] = cosf(fTheta * 0.5f);
+  matRotX.m[1][2] = sinf(fTheta * 0.5f);
+  matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+  matRotX.m[2][2] = cosf(fTheta * 0.5f);
+  matRotX.m[3][3] = 1;
+}
+
+
+void printMatrix(struct mat m) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      printw("%f\t", m.m[i][j]);
+    }
+    printw("\n");
+  }
 }
--- main.c
+++ main.c
@@ -3,20 +3,81 @@
 #include <pthread.h>
 #include <unistd.h>
 int main() {
-  struct Point p1 = {50, 70};
-  struct Point p2 = {14, 5};
-  struct Point p3 = {7, 10};
-  struct Triangle t1 = {p1, p2, p3};
-  struct Mesh mesh = {0};
-  mesh.Triangles[0] = t1;
-  mesh.size++;
-  initScreen();
-  while (1) {
+  struct Mesh mesh;
+  struct Point p1 = {0.0f, 0.0f, 0.0f};
 
-    drawMesh(mesh);
-    clearScreen();
-    usleep(16*1000);
-  }
+  struct Point p2 = {0.0f, 1.0f, 0.0f};
+  
+  struct Point p3 = {1.0f, 1.0f, 0.0f};
+  
+  struct Point p4 = {1.0f, 0.0f, 0.0f};
+  
+  struct Point p5 = {1.0f, 1.0f, 1.0f};
+  
+  struct Point p6 = {1.0f, 0.0f, 1.0f};
+  
+  struct Point p7 = {0.0f, 0.0f, 1.0f};
+  
+  struct Point p8 = {0.0f, 1.0f, 1.0f};
+  
+  struct Triangle t1 = {p1,p2,p3};
+  
+  struct Triangle t2 = {p1,p3,p4};
+
+  struct Triangle t3 = {p4,p3,p5};
+
+  struct Triangle t4 = {p4,p5,p6};
+
+  struct Triangle t5 = {p6,p5,p8};
+
+  struct Triangle t6 = {p6,p8,p7};
+
+  struct Triangle t7 = {p7,p8,p2};
+
+  struct Triangle t8 = {p7,p2,p1};
+
+  struct Triangle t9 = {p2,p8,p5};
+
+  struct Triangle t10 = {p2,p5,p3};
+
+  struct Triangle t11 = {p6,p7,p1};
+
+  struct Triangle t12 = {p6,p1,p4};
+  
+  mesh.Triangles[0] = t1;
+  mesh.Triangles[1] = t2;
+  mesh.Triangles[2] = t3;
+  mesh.Triangles[3] = t4;
+  mesh.Triangles[4] = t5;
+  mesh.Triangles[5] = t6;
+  mesh.Triangles[6] = t7;
+  mesh.Triangles[7] = t8;
+  mesh.Triangles[8] = t9;
+  mesh.Triangles[9] = t10;
+  mesh.Triangles[10] = t11;
+  mesh.Triangles[11] = t12;
+  mesh.size = 12;
+  initScreen();
+  // while (1) {
+  //   drawMesh(mesh);
+  //   clearScreen();
+  //   usleep(16 * 1000);
+  // }
+
+  // Initialize matrices
+  initMat();
+
+  // Print matrices
+  printw("Projection Matrix:\n");
+  printMatrix(matProj);
+  printw("\nRotation Matrix Z:\n");
+  printMatrix(matRotZ);
+  printw("\nRotation Matrix X:\n");
+  printMatrix(matRotX);
+
+  getch(); // Wait for user input before exiting
+  endwin(); // End ncurses mode
+
   return 0;
   //  startGame();
 }
