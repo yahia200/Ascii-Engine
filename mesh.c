29c7f9594d76273ccc0c0a5d49b1e552454915a2
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-06-11 01:47:15 +0300

    optimization

--- mesh.c
+++ mesh.c
@@ -2,8 +2,5 @@
 void drawMesh(struct Mesh *mesh, int fill) {
   sortTri(mesh->Triangles);
   place = 0;
-  for (int i = 0; i < mesh->size; i++) {
-    drawTriangle(&mesh->Triangles[i], fill);
-  }
+    drawTriangle(mesh, fill);
 }
-
--- triangles.c
+++ triangles.c
@@ -205,7 +205,8 @@ void clipp(struct Triangle t, int fill) {
   }
 }
 
-void drawTriangle(struct Triangle *triangle, int fill) {
+void drawTriangle(struct Mesh *m, int fill) {
+  int size = 0;
   struct Triangle triProjected, triTransformed, triViewed;
   struct mat matProj, matRotZ, matRotX, matRotY, matWorld;
   Matrix_MakeRotationZ(fThetaz, &matRotZ);
@@ -220,9 +221,8 @@ void drawTriangle(struct Triangle *triangle, int fill) {
   Matrix_MultiplyMatrix(matWorld, matRotY, &matWorld); // Transform by rotation
   Matrix_MultiplyMatrix(matWorld, matTrans,
                         &matWorld); // Transform by translation
-  
-  matW = matWorld;
 
+  matW = matWorld;
 
   struct Point pUp = {0, 1, 0, 1};
   lookDir.x = 0;
@@ -237,75 +237,80 @@ void drawTriangle(struct Triangle *triangle, int fill) {
 
   struct mat matView;
   Matrix_QuickInverse(matCamera, &matView);
+  for (int i = 0; i < m->size; i++) {
+    Matrix_MultiplyPoint(matWorld, m->Triangles[i].p[0], &triTransformed.p[0]);
+    Matrix_MultiplyPoint(matWorld, m->Triangles[i].p[1], &triTransformed.p[1]);
+    Matrix_MultiplyPoint(matWorld, m->Triangles[i].p[2], &triTransformed.p[2]);
 
-  Matrix_MultiplyPoint(matWorld, triangle->p[0], &triTransformed.p[0]);
-  Matrix_MultiplyPoint(matWorld, triangle->p[1], &triTransformed.p[1]);
-  Matrix_MultiplyPoint(matWorld, triangle->p[2], &triTransformed.p[2]);
+    struct Point normal, line1, line2;
+    Point_Sub(triTransformed.p[1], triTransformed.p[0], &line1);
+    Point_Sub(triTransformed.p[2], triTransformed.p[0], &line2);
 
-  struct Point normal, line1, line2;
-  Point_Sub(triTransformed.p[1], triTransformed.p[0], &line1);
-  Point_Sub(triTransformed.p[2], triTransformed.p[0], &line2);
+    // Take cross product of lines to get normal to triangle surface
+    Point_CrossProduct(line1, line2, &normal);
 
-  // Take cross product of lines to get normal to triangle surface
-  Point_CrossProduct(line1, line2, &normal);
+    // You normally need to normalise a normal!
+    normal = Point_Normalise(normal);
+    struct Point vCameraRay;
+    Point_Sub(triTransformed.p[0], camera, &vCameraRay);
+    // printw("%f  %f  %f  %f\n",normal.x, normal.y,normal.z, normal.w);
 
-  // You normally need to normalise a normal!
-  normal = Point_Normalise(normal);
-  struct Point vCameraRay;
-  Point_Sub(triTransformed.p[0], camera, &vCameraRay);
-  // printw("%f  %f  %f  %f\n",normal.x, normal.y,normal.z, normal.w);
+    // printw("  %f\n", Point_DotProduct(normal, vCamera));
+    // printw("\n%f  %f  %f  %f", triTransformed.p[0].x, triTransformed.p[0].y,
+    // triTransformed.p[0].z, triTransformed.p[0].w);
+    if (Point_DotProduct(normal, vCameraRay) < 0.0f) {
+      struct Point light_direction = {0.0f, 0.0f, -1.0f, 1};
+      light_direction = Point_Normalise(light_direction);
 
-  // printw("  %f\n", Point_DotProduct(normal, vCamera));
-  // printw("\n%f  %f  %f  %f", triTransformed.p[0].x, triTransformed.p[0].y,
-  // triTransformed.p[0].z, triTransformed.p[0].w);
-  if (Point_DotProduct(normal, vCameraRay) < 0.0f) {
-    struct Point light_direction = {0.0f, 0.0f, -1.0f, 1};
-    light_direction = Point_Normalise(light_direction);
+      // How "aligned" are light direction and triangle surface normal?
+      float dp = fmax(0.1f, Point_DotProduct(light_direction, normal));
 
-    // How "aligned" are light direction and triangle surface normal?
-    float dp = fmax(0.1f, Point_DotProduct(light_direction, normal));
+      int intensity = (int)(dp * brightnessRange);
+      // view
+      Matrix_MultiplyPoint(matView, triTransformed.p[0], &triViewed.p[0]);
+      Matrix_MultiplyPoint(matView, triTransformed.p[2], &triViewed.p[1]);
+      Matrix_MultiplyPoint(matView, triTransformed.p[1], &triViewed.p[2]);
+      int nClipped = 0;
+      struct Triangle clipped[2];
+      nClipped = Triangle_ClipAgainstPlane(
+          (struct Point){0, 0, 0.1, 1}, (struct Point){0, 0, 1, 1}, &triViewed,
+          &clipped[0], &clipped[1]);
 
-    int intensity = (int)(dp * brightnessRange);
-    // view
-    Matrix_MultiplyPoint(matView, triTransformed.p[0], &triViewed.p[0]);
-    Matrix_MultiplyPoint(matView, triTransformed.p[2], &triViewed.p[1]);
-    Matrix_MultiplyPoint(matView, triTransformed.p[1], &triViewed.p[2]);
-    int nClipped = 0;
-    struct Triangle clipped[2];
-    nClipped = Triangle_ClipAgainstPlane((struct Point){0, 0, 0.1, 1},
-                                         (struct Point){0, 0, 1, 1}, &triViewed,
-                                         &clipped[0], &clipped[1]);
+      // printw("%d", nClipped);
+      // printw("\n%f  %f %f  %f", clipped[0].p[0].x, clipped[0].p[0].y,
+      // clipped[0].p[0].z, clipped[0].p[0].w);
+      for (int n = 0; n < nClipped; n++) {
+        // Project
+        Matrix_MultiplyPoint(matProj, clipped[n].p[0], &triProjected.p[0]);
+        Matrix_MultiplyPoint(matProj, clipped[n].p[1], &triProjected.p[1]);
+        Matrix_MultiplyPoint(matProj, clipped[n].p[2], &triProjected.p[2]);
 
-    // printw("%d", nClipped);
-    // printw("\n%f  %f %f  %f", clipped[0].p[0].x, clipped[0].p[0].y,
-    // clipped[0].p[0].z, clipped[0].p[0].w);
-    for (int n = 0; n < nClipped; n++) {
-      // Project
-      Matrix_MultiplyPoint(matProj, clipped[n].p[0], &triProjected.p[0]);
-      Matrix_MultiplyPoint(matProj, clipped[n].p[1], &triProjected.p[1]);
-      Matrix_MultiplyPoint(matProj, clipped[n].p[2], &triProjected.p[2]);
-
-      // normalize
-      Point_Div(triProjected.p[0], triProjected.p[0].w, &triProjected.p[0]);
-      Point_Div(triProjected.p[1], triProjected.p[1].w, &triProjected.p[1]);
-      Point_Div(triProjected.p[2], triProjected.p[2].w, &triProjected.p[2]);
-      // printw("%f  %f  %f  %f\n", triProjected.p[0].x, triProjected.p[0].y,
-      // triProjected.p[0].z, triProjected.p[0].w);
-      // offset
-      struct Point vOffsetView = {1, 1, 0, 1};
-      Point_Add(triProjected.p[0], vOffsetView, &triProjected.p[0]);
-      Point_Add(triProjected.p[1], vOffsetView, &triProjected.p[1]);
-      Point_Add(triProjected.p[2], vOffsetView, &triProjected.p[2]);
-      triProjected.p[0].x *= 0.5f * (float)MAX_WIDTH;
-      triProjected.p[0].y *= 0.5f * (float)MAX_HIEGHT;
-      triProjected.p[1].x *= 0.5f * (float)MAX_WIDTH;
-      triProjected.p[1].y *= 0.5f * (float)MAX_HIEGHT;
-      triProjected.p[2].x *= 0.5f * (float)MAX_WIDTH;
-      triProjected.p[2].y *= 0.5f * (float)MAX_HIEGHT;
-      triProjected.intensity = intensity;
-      rasterTriangle(triProjected, fill);
-      // copyTriangle(&triProjected, &triToDraw[place++]);
-      // clipp(triProjected, fill);
+        // normalize
+        Point_Div(triProjected.p[0], triProjected.p[0].w, &triProjected.p[0]);
+        Point_Div(triProjected.p[1], triProjected.p[1].w, &triProjected.p[1]);
+        Point_Div(triProjected.p[2], triProjected.p[2].w, &triProjected.p[2]);
+        // printw("%f  %f  %f  %f\n", triProjected.p[0].x, triProjected.p[0].y,
+        // triProjected.p[0].z, triProjected.p[0].w);
+        // offset
+        struct Point vOffsetView = {1, 1, 0, 1};
+        Point_Add(triProjected.p[0], vOffsetView, &triProjected.p[0]);
+        Point_Add(triProjected.p[1], vOffsetView, &triProjected.p[1]);
+        Point_Add(triProjected.p[2], vOffsetView, &triProjected.p[2]);
+        triProjected.p[0].x *= 0.5f * (float)MAX_WIDTH;
+        triProjected.p[0].y *= 0.5f * (float)MAX_HIEGHT;
+        triProjected.p[1].x *= 0.5f * (float)MAX_WIDTH;
+        triProjected.p[1].y *= 0.5f * (float)MAX_HIEGHT;
+        triProjected.p[2].x *= 0.5f * (float)MAX_WIDTH;
+        triProjected.p[2].y *= 0.5f * (float)MAX_HIEGHT;
+        triProjected.intensity = intensity;
+        // rasterTriangle(triProjected, fill);
+        copyTriangle(&triProjected, &triToDraw[size++]);
+        // clipp(triProjected, fill);
+      }
     }
+    
   }
+for (int i = 0; i < size; i++) {
+      rasterTriangle(triToDraw[i], fill);
+    }
 }
